99c74804b9a64b5727f44353977e3d6cb744616a
Tanner Helton <tanner@tannerhelton.com>
2023-04-07 21:46:55 -0500

    new testing scripts

--- Lab05_HashTable/test.sh
+++ Lab05_HashTable/test.sh
@@ -1,10 +1,24 @@
 g++ -std=c++11 MainTest.cpp -o Lab &&
+
+echo "Old test cases:"
+
 for i in {0..9}; do
-    ./Lab "Inputs/input_$i.txt" > "result_$i.txt"
+    ./Lab "OLD_Inputs/input_$i.txt" > "result_$i.txt"
 done
 
 for i in {0..9}; do
-    python3 GradingScript.py "result_$i.txt" "Outputs/output_$i.txt"
+    python3 GradingScript.py "result_$i.txt" "OLD_Outputs/output_$i.txt"
+    rm "result_$i.txt"
+done
+
+echo "New test cases:"
+
+for i in {0..9}; do
+    ./Lab "NEW_Inputs/input_$i.txt" > "result_$i.txt"
+done
+
+for i in {0..9}; do
+    python3 GradingScript.py "result_$i.txt" "NEW_Outputs/output_$i.txt"
     rm "result_$i.txt"
 done
 
--- Lab05_HashTable/valgrindTest.sh
+++ Lab05_HashTable/valgrindTest.sh
@@ -1,11 +1,24 @@
 g++ -std=c++11 MainTest.cpp -o Lab
 
+echo "Old tests:"
+
 for i in {0..9}; do
-    valgrind --leak-check=full ./Lab "Inputs/input_$i.txt" > "result_$i.txt"
+    valgrind --leak-check=full ./Lab "OLD_Inputs/input_$i.txt" > "result_$i.txt"
 done
 
 for i in {0..9}; do
-    python3 GradingScript.py "result_$i.txt" "Outputs/output_$i.txt"
+    python3 GradingScript.py "result_$i.txt" "OLD_Outputs/output_$i.txt"
+    rm "result_$i.txt"
+done
+
+echo "New tests:"
+
+for i in {0..9}; do
+    valgrind --leak-check=full ./Lab "NEW_Inputs/input_$i.txt" > "result_$i.txt"
+done
+
+for i in {0..9}; do
+    python3 GradingScript.py "result_$i.txt" "NEW_Outputs/output_$i.txt"
     rm "result_$i.txt"
 done
 
