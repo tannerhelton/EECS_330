4a804f368c36cdcd9b29e8b3d8f13c4b952011b6
Tanner Helton <tanner@tannerhelton.com>
2023-04-10 14:57:22 -0500

    create tests

---
+++ Lab06_BST/valgrindTest.sh
@@ -0,0 +1,12 @@
+g++ -std=c++11 MainTest.cpp -o Lab
+
+for i in {0..9}; do
+    valgrind --leak-check=full ./Lab "Inputs/input_$i.txt" > "result_$i.txt"
+done
+
+for i in {0..9}; do
+    python3 GradingScript.py "result_$i.txt" "Outputs/output_$i.txt"
+    rm "result_$i.txt"
+done
+
+rm Lab
