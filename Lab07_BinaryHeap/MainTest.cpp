d3ae4f50cc2a775cdd96d6015ff26d22c6bdc4c0
Tanner Helton <tanner@tannerhelton.com>
2023-04-24 13:07:47 -0500

    add test sh

--- Lab07_BinaryHeap/MainTest.cpp
+++ Lab07_BinaryHeap/MainTest.cpp
@@ -3,11 +3,11 @@
 #include <string>
 #include <vector>
 
-#include "MyBinaryHeap.h"
+#include "MyBinaryHeap_t984h395.h"
 
 using namespace std;
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
@@ -23,58 +23,59 @@ int main(int argc, char* argv[])
     {
         // testing insert
         string str;
-        while(getline(inFile, str))
+        while (getline(inFile, str))
         {
             int num = stoi(str);
             bh.enqueue(static_cast<unsigned int>(num));
-            cout << "Data inserted." << "\t";
-            cout << "Highest priority:" << bh.front() << endl;            
+            cout << "Data inserted."
+                 << "\t";
+            cout << "Highest priority:" << bh.front() << endl;
         }
 
         // testing heap property
-        if(bh.verifyHeapProperty())
+        if (bh.verifyHeapProperty())
             cout << "Heap property checked." << endl;
         else
             cout << "Heap property violated." << endl;
 
         // testing increaseKey and decreaseKey
         MyBinaryHeap<unsigned int> bh2 = bh;
-        
-        for(size_t i = 0; i < 5000; ++ i)
+
+        for (size_t i = 0; i < 5000; ++i)
         {
-            size_t p = 1 + ((int) rand()) % bh2.size();
-            unsigned int d = ((int) rand()) % 999999;
-            int inc_tag = ((int) rand()) % 2;
-            if(inc_tag)
+            size_t p = 1 + ((int)rand()) % bh2.size();
+            unsigned int d = ((int)rand()) % 999999;
+            int inc_tag = ((int)rand()) % 2;
+            if (inc_tag)
                 bh2.increaseKey(p, d);
             else
                 bh2.decreaseKey(p, d);
         }
-        if(bh2.verifyHeapProperty())
+        if (bh2.verifyHeapProperty())
             cout << "Heap property checked." << endl;
         else
             cout << "Heap property violated." << endl;
 
         // testing merge
         bh.disruptHeapProperty();
-        if(bh.verifyHeapProperty())
+        if (bh.verifyHeapProperty())
             cout << "Heap property checked." << endl;
         else
             cout << "Heap property violated." << endl;
         bh2.disruptHeapProperty();
-        if(bh2.verifyHeapProperty())
+        if (bh2.verifyHeapProperty())
             cout << "Heap property checked." << endl;
         else
             cout << "Heap property violated." << endl;
 
         bh.merge(move(bh2));
-        if(bh.verifyHeapProperty())
+        if (bh.verifyHeapProperty())
             cout << "Heap property checked." << endl;
         else
             cout << "Heap property violated." << endl;
 
         // testing front and dequeue
-        while(!bh.empty())
+        while (!bh.empty())
         {
             unsigned int v = bh.front();
             cout << "Highest priority: " << v << endl;
@@ -83,7 +84,7 @@ int main(int argc, char* argv[])
     }
     else
     {
-            cout << "The input file cannot be opened." << endl;
+        cout << "The input file cannot be opened." << endl;
     }
     inFile.close();
     return 0;
